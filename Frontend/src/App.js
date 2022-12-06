3257f6ad384b1a51aebce6166e2e01b97023da6a
Hamzaali565 <ha6153806@gmail.com>
2022-12-06 22:41:05 +0500

    API ready and frontend Completed

---
+++ Frontend/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+// import './App.css';
+import Navbar from './components/Navbar';
+import { Stack } from '@mui/system';
+
+function App() {
+  return (
+    <Stack>
+      <Navbar />
+    </Stack>
+  );
+}
+
+export default App;
